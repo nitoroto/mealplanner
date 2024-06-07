a9ace485cadca9e32835582e0fde81514dd99a2d
nitoroto <161154942+nitoroto@users.noreply.github.com>
2024-06-07 11:15:19 +0200

    change names

--- test/mealTests.js
+++ test/mealTests.js
@@ -2,63 +2,63 @@ const request = require('supertest');
 const app = require('../app');
 require('dotenv').config();
 
-describe('Meal Management Functionalities', () => {
-  const token = `Bearer ${process.env.TEST_USER_TOKEN}`;
+describe('Meal Management Functionalities Tests', () => {
+  const authToken = `Bearer ${process.env.TEST_USER_TOKEN}`;
 
-  const newMeal = {
+  const mealToCreate = {
     name: 'Grilled Chicken',
     calories: 500,
     date: new Date(),
   };
 
-  const updatedMeal = {
+  const mealToUpdateData = {
     name: 'Updated Meal Name',
     calories: 600,
   };
 
-  const mealIdToUpdate = 'someMealId';
-  const mealIdToDelete = 'someMealIdToDelete';
+  const mealIdForUpdate = 'someMealIdToUpdate';
+  const mealIdForDeletion = 'someMealIdToDelete';
 
-  it('should create a new meal', async () => {
-    const res = await request(app)
+  it('should create a new meal successfully', async () => {
+    const response = await request(app)
       .post('/meals')
-      .send(newMeal)
-      .set('Authorization', token);
+      .send(mealToCreate)
+      .set('Authorization', authToken);
 
-    expect(res.statusCode).toEqual(201);
-    expect(res.body).toHaveProperty('id');
+    expect(response.statusCode).toEqual(201);
+    expect(response.body).toHaveProperty('id');
   });
 
-  it('should retrieve all meals', async () => {
-    const res = await request(app)
+  it('should fetch all meals', async () => {
+    const response = await request(app)
       .get('/meals')
-      .set('Authorization', token);
+      .set('Authorization', authToken);
 
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toBeInstanceOf(Array);
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toBeInstanceOf(Array);
   });
 
-  it('should update a meal', async () => {
-    const res = await request(app)
-      .put(`/meals/${mealIdToUpdate}`)
-      .send(updatedMeal)
-      .set('Authorization', token);
+  it('should successfully update a meal', async () => {
+    const response = await request(app)
+      .put(`/meals/${mealIdForUpdate}`)
+      .send(mealToUpdateData)
+      .set('Authorization', authToken);
 
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('id', meal: IdToUpdate);
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toHaveProperty('id', mealIdForUpdate);
   });
 
-  it('should delete a meal', async () => {
-    const res = await request(app)
-      .delete(`/meals/${mealIdToDelete}`)
-      .set('Authorization', token);
+  it('should delete a meal successfully', async () => {
+    const response = await request(app)
+      .delete(`/meals/${mealIdForDeletion}`)
+      .set('Authorization', authToken);
 
-    expect(res.statusCode).toEqual(204);
+    expect(response.statusCode).toEqual(204);
   });
 
-  it('should not allow unauthorized access to manage meals', async () => {
-    const res = await request(app).get('/meals');
+  it('should restrict unauthorized access to meal management', async () => {
+    const response = await request(app).get('/meals');
 
-    expect(res.statusCode).toEqual(401);
+    expect(response.statusCode).toEqual(401);
   });
 });
